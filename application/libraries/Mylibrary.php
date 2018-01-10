a9d2c7b8f8c12b1780879db4b90da18e676164de
sabeer zabi <sabeerzabi@gamil.com>
2018-01-10 11:25:48 +0530

    Codignator basics

---
+++ application/libraries/Mylibrary.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Mylibrary {
+    public function some_function()
+    {
+    }
+}
+/* End of file Mylibrary.php */
+
